ff11af226e5655aee608fd68283c2675a7db3d0a
Khanya45 <khanyagope93@gmail.com>
2021-09-01 16:09:17 +0200

    updated file

--- Javascript/property_det.js
+++ Javascript/property_det.js
@@ -32,7 +32,58 @@ function displayPropertyInfo(){
         let agent = agents.find((person) => {
             return person[0] == property[4];
         });
+        let form_container = document.querySelector(".contact-form")
         let agent_coontainer = document.querySelector(".call_agent")
+        form_container.innerHTML = `
+        <div id="heading">
+        <label>Contact Agent</label>
+    </div>
+    <div>
+        <input
+            type="text"
+            required
+            placeholder="Enter your fullname"
+            class="contact-info"
+        />
+        </label>
+    </div>
+    <div>
+        
+        <!-- Surname: -->
+        <input
+            class="contact-info"
+            type="surname"
+            name="surname"
+            id="surname"
+            required
+            placeholder="Enter your phone number"
+        />
+        </label>
+    </div>
+    <div>
+        <label for="email">
+        <!-- Your email: -->
+        <input
+            class="contact-info"
+            type="email"
+            name="_replyto"
+            id="email"
+            required
+            placeholder="Enter your email"
+        />
+        </label>
+    </div>
+    <div>
+        <label for="message">
+        <!-- Your message: -->
+        <textarea
+            name="message"
+            id="text-box"
+            placeholder="Enter your message"
+        ></textarea>
+        </label>
+    </div>   
+    <button onclick="send_email(${agent[0]})" id="submit-btn">Email Agent</button>`
         agent_coontainer.innerHTML = `
                 <img src="${agent[3]}">
                 <h3>${agent[4]}</h3>
@@ -46,4 +97,25 @@ function displayPropertyInfo(){
 
 displayPropertyInfo();
 
-// property_id
+
+function send_email(id){
+  info = []
+  let inputs = document.getElementsByTagName("input")
+  for (index=0;index<inputs.length;index++){
+     info.push(inputs[index].value)
+  }
+  let textarea = document.getElementById("text-box").value
+  body = `I, ${info[0]}\n ${textarea} \n Here my contacts:\n Mobile:${info[1]}\n email:${info[2]}`
+  // console.log(JSON.stringify(body))
+  fetch("http://127.0.0.1:5002/send-email/"+id+"/"+body+"/", {
+        method: "POST",
+        body: info[2],
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+      },
+        })
+    .then((res) => res.json())
+    .then((response) => {
+      console.log(response.data)
+    })
+}
--- Javascript/propertylist.js
+++ Javascript/propertylist.js
@@ -54,28 +54,25 @@ function filterAll(){
             checkedBox.push(checkBox[index].value)
         }       
     }
-    // console.log(checkedBox)
-    for (index=0; index<checkedBox.length;index++){
-        fetch("https://desolate-retreat-38151.herokuapp.com/property-by-type/"+checkedBox[index]+"/", {
-        method: "get",
-        })
-        .then((res) => res.json())
-        .then((json) => {
-            arrProp = json.data  
-            for (i=0; i<arrProp.length;i++){
-                displayProperty(filteredList)
-           filteredList.push(arrProp[i])
-
-            }
-        })
+    if (checkedBox.length == 3){
+        filterBySuburb();
     }
-    // filteredList.forEach(property=>{
-    //     console.log(property)
-    // })
-    // console.log(filteredList)
-    console.log(filteredList)
-    // displayProperty(filteredList)
-    
+    else{
+        for (index=0; index<checkedBox.length;index++){
+            fetch("https://desolate-retreat-38151.herokuapp.com/property-by-type/"+checkedBox[index]+"/", {
+            method: "get",
+            })
+            .then((res) => res.json())
+            .then((json) => {
+                arrProp = json.data  
+                for (i=0; i<arrProp.length;i++){
+                    filteredList.push(arrProp[i])
+                    displayProperty(filteredList)
+                }
+            })
+        }
+        console.log(filteredList)
+    }   
 }
 
 function redirectToInfo(id){
@@ -83,3 +80,39 @@ function redirectToInfo(id){
     window.location.replace("http://127.0.0.1:5501/property_det.html")
 }
 
+function filterByPrice(){
+    let suburb = localStorage.getItem("filterSuburb");
+    let price = []
+    let checkBox = document.getElementsByTagName("input")
+    for (index=0; index<2;index++){
+        price.push(checkBox[index].value)  
+    }
+    if (price.length=2){
+        fetch("http://127.0.0.1:5002/filter-by-price/"+price[0]+"/"+price[1]+"/"+suburb+"/", {
+            method: "get",
+            })
+            .then((res) => res.json())
+            .then((json) => 
+            {
+                console.log(json.data)
+                // price_filtered = json.data
+                // let type_filtered = price_filtered.filter(property=> {return property[]})
+            })
+    }
+}
+
+// filterByPrice();
+
+
+function filterPropertybyPrice(){
+    let checkedBox = []
+    let checkBox = document.getElementsByTagName("input")
+        for (index=0; index<2;index++){
+            checkedBox.push(checkBox[index].value)  
+        }
+    let filtered = arrProperties.filter(property=>{
+        return property[3] >= checkedBox[0] && property[3] <= checkedBox[2]
+    })
+    console.log(arrProperties[0])
+    // arrProperties.forEach((property)=>{console.log(property[3])})
+}
