1b32b3e30911c9b306583cfc20c32c1a0e60c381
Khanya45 <khanyagope93@gmail.com>
2021-09-02 16:23:36 +0200

    updated file

--- Javascript/dynamic.js
+++ Javascript/dynamic.js
@@ -2,6 +2,20 @@
 function filterBySuburb(){
   let suburb = document.querySelector("input").value
   localStorage.filterSuburb = suburb;
+
+}
+
+function filterByBuy(){
+  if(document.getElementById('btnBuy').clicked == true)
+  {
+     alert("button was clicked");
+  }
 }
 
 
+function filterByRent(){
+  if(document.getElementById('btnRent').clicked == true)
+  {
+     alert("button was clicked");
+  }
+}
